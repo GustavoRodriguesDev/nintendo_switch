e4aae61906be7858a8813a8780c3af2684097195
gustavokg1999 <gustavorpsantos1999@gmail.com>
2021-12-21 09:13:03 -0300

    responsividade do item controle e tela esta funcionando

--- lib/nintendo.dart
+++ lib/nintendo.dart
@@ -14,35 +14,63 @@ class NintendoSwitch extends StatefulWidget {
 class _NintendoSwitchState extends State<NintendoSwitch> {
   @override
   Widget build(BuildContext context) {
-    double width = double.maxFinite;
-    double height = double.maxFinite;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Container(
         color: Color(0xFF32383A),
         width: width,
         height: height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            // BigButton(size: 59),
-            // SoundButton(),
-            // HomeButton(size: 29),
-            // DirectionalButton(),
-            // ActionButton(),
-            // PLusButton(),
-            // BtnMenos(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Keybord(side: Side.left),
-                LampGroup(),
-                FlutterLogo(),
-                LampGroup(),
-                Keybord(side: Side.right),
-              ],
-            ),
-          ],
-        ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 25,
+                  left: 22,
+                  right: 22,
+                ),
+                child: Container(
+                  height: height * 0.53,
+                  width: width * 0.88,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Color(0xFF000000)),
+                ),
+              ),
+              // BigButton(size: 59),
+              // SoundButton(),
+              // HomeButton(size: 29),
+              // DirectionalButton(),
+              // ActionButton(),
+              // PLusButton(),
+              // BtnMenos(),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Keybord(
+                    side: Side.left,
+                    heigth: height * 0.38,
+                    width: width * 0.32,
+                  ),
+                  LampGroup(),
+                  FlutterLogo(),
+                  LampGroup(),
+                  Keybord(
+                    side: Side.right,
+                    heigth: height * 0.38,
+                    width: width * 0.32,
+                  ),
+                ],
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
