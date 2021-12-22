8ffae36adb7a57592422bb5124c676eeb04d3b4b
gustavokg1999 <gustavorpsantos1999@gmail.com>
2021-12-22 10:12:15 -0300

    melhorias de responsividade

--- lib/componentes/directionalButton.dart
+++ lib/componentes/directionalButton.dart
@@ -1,14 +1,15 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, file_names, unused_import, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, file_names, unused_import, sized_box_for_whitespace, must_be_immutable
 
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:nintendo_switch/componentes/componente%20bot%C3%B5es/icon_action.dart';
 
 import 'componente botões/botaoPequeno.dart';
 
 class DirectionalButton extends StatelessWidget {
   double size;
-  DirectionalButton({required this.size});
+  DirectionalButton({required this.size, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,39 +21,41 @@ class DirectionalButton extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             BotaoPequeno(
-              type: Type.icon,
+              align: Position.up,
               size: constraints.maxWidth * 0.32,
-              sizeIcom: 25,
-              gradientBegin: Alignment.bottomCenter,
-              gradientEnd: Alignment.topCenter,
-              angule: 270,
+              typeAction: IconsActions(
+                angle: 270,
+                sizeIcons: constraints.maxWidth * 0.19,
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 BotaoPequeno(
-                    type: Type.icon,
-                    size: constraints.maxWidth * 0.32,
-                    sizeIcom: 25,
-                    gradientBegin: Alignment.bottomCenter,
-                    gradientEnd: Alignment.topCenter,
-                    angule: 180),
+                  align: Position.right,
+                  size: constraints.maxWidth * 0.32,
+                  typeAction: IconsActions(
+                    angle: 180,
+                    sizeIcons: constraints.maxWidth * 0.19,
+                  ),
+                ),
                 BotaoPequeno(
-                    type: Type.icon,
-                    size: constraints.maxWidth * 0.32,
-                    sizeIcom: 25,
-                    gradientBegin: Alignment.centerRight,
-                    gradientEnd: Alignment.centerLeft,
-                    angule: 0),
+                  align: Position.left,
+                  size: constraints.maxWidth * 0.32,
+                  typeAction: IconsActions(
+                    angle: 0,
+                    sizeIcons: constraints.maxWidth * 0.19,
+                  ),
+                ),
               ],
             ),
             BotaoPequeno(
-              type: Type.icon,
+              align: Position.down,
               size: constraints.maxWidth * 0.32,
-              sizeIcom: 25,
-              gradientBegin: Alignment.centerRight,
-              gradientEnd: Alignment.centerLeft,
-              angule: 90,
+              typeAction: IconsActions(
+                angle: 90,
+                sizeIcons: constraints.maxWidth * 0.19,
+              ),
             ),
           ],
         );
